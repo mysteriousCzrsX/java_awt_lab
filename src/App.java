2173d2d5c587da107d18c64176fdf7d512710fe1
mysteriousCzrsX <cezartusken47@gmail.com>
2023-04-20 10:05:52 +0200

    bruh what

--- src/App.java
+++ src/App.java
@@ -1,16 +1,6 @@
-import java.awt.*;
 public class App {
     public static void main(String[] args){
         TestFrame tf = new TestFrame();
-        tf.init();
-        while(true){
-            if(tf.black.isSelected()){
-                tf.drawPlane.setDrawColor(Color.BLACK);
-            }
-            if(tf.red.isSelected()){
-                tf.drawPlane.setDrawColor(Color.RED);
-            }
-            //System.out.println(tf.drawPlane.keyPress);
-        }
+        tf.setVisible(true);
     }
 }
--- src/TestFrame.java
+++ src/TestFrame.java
@@ -1,20 +1,31 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class TestFrame extends JFrame{
-    public JCheckBox black = new JCheckBox("czarny");
-    public JCheckBox red = new JCheckBox("czerwony");
-    public LineCanvas drawPlane = new LineCanvas();
+    private JCheckBox black = new JCheckBox("czarny");
+    private JCheckBox red = new JCheckBox("czerwony");
+    private LineCanvas drawPlane = new LineCanvas();
 
-    public void init(){
-        setLayout(new GridLayout());
+    TestFrame(){
+        setLayout(new GridLayout(1, 2));
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         add(black);
         add(red);
-        drawPlane.init();
+        red.addActionListener((ActionEvent e) -> {
+            if (red.isSelected()) {
+                drawPlane.setDrawColor(Color.red);
+            }
+        });
+
+        black.addActionListener((ActionEvent e) -> {
+            if (black.isSelected()) {
+                drawPlane.setDrawColor(Color.black);
+            }
+        });
+
         add(drawPlane);
         setFocusable(true);
-        setVisible(true);
     }
 }
