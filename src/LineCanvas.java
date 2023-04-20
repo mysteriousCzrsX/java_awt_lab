10605c31d8f9b652bf0bdf14ca97a4afa021510f
mysteriousCzrsX <cezartusken47@gmail.com>
2023-04-20 10:03:45 +0200

    drawing and color working

--- src/LineCanvas.java
+++ src/LineCanvas.java
@@ -1,38 +1,34 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.Line2D;
 import java.util.ArrayList;
+
 public class LineCanvas extends Canvas {
     public ArrayList<Integer> keyPressHistory = new ArrayList<Integer>();
-    public int keyPress = 0;
-    public Color drawColor = Color.BLACK;
 
-    private int prevX = getWidth()/2;
-    private int prevY = getHeight()/2;
+    private Color drawColor = Color.BLACK;
+    private int prevX = 0;
+    private int prevY = 0;
     private int nextX = 0;
     private int nextY = 0;
 
 
     LineCanvas(){
-        
+        setBackground(Color.gray);
+        setVisible(true);
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 int key = e.getKeyCode();
-                setKeyPress(key);
                 keyPressHistory.add(key);
-                repaint();
+                drawLines(key);
             }
         });
     }
-    public void init(){
-        setBackground(Color.gray);
-        setVisible(true);
-    }
 
-    public void paint(Graphics g){
+    private void drawLines(int key){
+        Graphics g = getGraphics();
         g.setColor(drawColor);
-        switch (keyPress) {
+        switch (key) {
             case KeyEvent.VK_UP:
                 nextY = prevY - 5;
                 break;
@@ -49,14 +45,8 @@ public class LineCanvas extends Canvas {
                 break;
         }
         g.drawLine(prevX, prevY, nextX, nextY);
-        g.drawLine(10, 10, 90, 90);
         prevX = nextX;
         prevY = nextY;
-        keyPress = 0;
-    }
-
-    public void setKeyPress(int k){
-        keyPress = k;
     }
 
     public void setDrawColor(Color c){
